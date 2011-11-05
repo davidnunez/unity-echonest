e3791b321d828d1e3630baa69c4e7a1a791664ca
David Nunez <david@davidnunez.com>
2011-11-05 13:04:24 -0400

    First Commit

---
+++ Assets/Editor/EchoNestEditor.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+ 
+[CustomEditor (typeof(EchoNest))]
+public class EchoNestEditor : Editor
+{
+	public override void OnInspectorGUI()
+    {
+    }
+}
